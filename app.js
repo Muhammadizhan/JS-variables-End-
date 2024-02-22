e634b1b15b221d0a4e45b6af962d0d52e566cd0a
Muhammadizhan <izhanayaz4@gmail.com>
2024-02-22 23:12:06 +0500

    special Srting

---
+++ app.js
@@ -0,0 +1,8 @@
+ // Store the given string in a variable
+ var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+ // Display the string in an alert
+ alert(specialString);
+
+ // Display the string in the browser
+ document.write("<p>" + specialString + "</p>");
